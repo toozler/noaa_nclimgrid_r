7bac4b354b08cd49e5b2fce999f44416a072462e
toozler <toozler@gmail.com>
2022-07-24 18:14:54 -0400

    Added stuff to demo

--- demo/demo.R
+++ demo/demo.R
@@ -69,3 +69,67 @@ plot_nclimgrid_histogram(m_p, n_p)
 plot_nclimgrid_histogram(a_p)
 
 #create .rds with pixels by state (intersect nclimgrid grid with maps polygons and store  )
+
+
+#daily preliminary grids - can be read and aggregated to get prcp prior to the official release
+# requires netcdf
+# https://www.ncei.noaa.gov/pub/data/daily-grids/beta/by-month/2022/07/
+
+#newly published ranks data
+# https://www.ncei.noaa.gov/access/monitoring/us-maps/1/202206?products[]=grid-ranks-tavg
+rks <- 'https://www.ncei.noaa.gov/pub/data/cirs/climgrid/us_tave_1mo.dat' %>%
+  read.delim(header=T, sep="")
+
+t_rank_names <- data.frame(cat = 1:7,
+                         description = c("Record Coldest",
+                                         "Much Below Avg",
+                                         "Below Average",
+                                         "Near Average",
+                                         "Above Average",
+                                         "Much Above Avg",
+                                         "Record Warmest"))
+
+rks %>%
+  #left_join(rank_names, by = "cat") %>%
+  ggplot() +
+  geom_raster(aes(x = lon, y = lat, fill = as.factor(cat))) +
+  scale_fill_manual(values = rev(brewer.pal(7, "RdBu")),
+                    drop = F,
+                    breaks = t_rank_names$cat,
+                    labels = t_rank_names$description,
+                    name = NULL) +
+  theme_minimal() +
+  geom_polygon(data = map_data("state"), aes(x = long, y = lat, group = group), color = "black", fill=NA, size=0.1) +
+  labs(x=NULL, y=NULL) +
+  theme(legend.position = 'bottom') +
+  guides(fill = guide_legend(nrow = 1))
+
+
+
+prcp_rks <- 'https://www.ncei.noaa.gov/pub/data/cirs/climgrid/us_prcp_3mo.dat' %>%
+  read.delim(header=T, sep="")
+
+prcp_rank_names <- data.frame(cat = 0:7,
+                              description = c('Record Driest (0")',
+                                              "Record Driest",
+                                              "Much Below Average",
+                                              "Below Average",
+                                              "Near Average",
+                                              "Above Average",
+                                              "Much Above Avg",
+                                              "Record Wettest"))
+
+prcp_rks %>%
+  #left_join(rank_names, by = "cat") %>%
+  ggplot() +
+  geom_raster(aes(x = lon, y = lat, fill = as.factor(cat))) +
+  scale_fill_manual(values = c("#BBBBBB", brewer.pal(7, "BrBG")),
+                    drop = F,
+                    breaks = prcp_rank_names$cat,
+                    labels = prcp_rank_names$description,
+                    name = NULL) +
+  theme_minimal() +
+  geom_polygon(data = map_data("state"), aes(x = long, y = lat, group = group), color = "black", fill=NA, size=0.1) +
+  labs(x=NULL, y=NULL) +
+  theme(legend.position = 'bottom') +
+  guides(fill = guide_legend(nrow = 1))
