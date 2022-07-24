64ad548574b407fb74307a82c90a6fd0c582a7a0
toozler <toozler@gmail.com>
2022-07-23 23:06:06 -0400

    Added title builder and color scale builder to plot function

--- demo/demo.R
+++ demo/demo.R
@@ -3,14 +3,59 @@ m_tave <- get_nclimgrid_monthly(2021, "tave", "conus", wide = FALSE, verbose = T
 n_tave <- get_nclimgrid_normals("1901-2000", "tave", "conus", wide = FALSE, verbose = TRUE)
 a_tave <- compute_anomaly(m_tave, n_tave)
 
-plot_measurement_data(m_tave, subset_months = 2:5, title = "Temperature", subtitle = "Test", show_credit = T)
+m_tave %>%
+  filter(month %in% 2:5) %>%
+  plot_nclimgrid()
+
+n_tave %>%
+  filter(month %in% 2:5) %>%
+  plot_nclimgrid()
+
+a_tave %>%
+  filter(month %in% 2:5) %>%
+  plot_nclimgrid()
 
 
-
+#testing tmax
 m_tmax <- get_nclimgrid_monthly(2022, "tmax", "conus", wide = FALSE, verbose = TRUE)
 n_tmax <- get_nclimgrid_normals("1901-2000", "tmax", "conus", wide = FALSE, verbose = TRUE)
 a_tmax <- compute_anomaly(m_tmax, n_tmax)
 
-plot_measurement_data(m_tmax, subset_months = 3:6, title = "Temperature Max", subtitle = "Test", show_credit = T)
+m_tmax %>%
+  filter(month %in% 6) %>%
+  plot_nclimgrid() +
+  theme_void() +
+  theme(legend.position = 'left')
 
+n_tmax %>%
+  filter(month %in% 6) %>%
+  plot_nclimgrid() +
+  theme_void() +
+  theme(legend.position = 'left')
+
+a_tmax %>%
+  filter(month %in% 4) %>%
+  plot_nclimgrid()
+
+
+
+# testing
+m_p <- get_nclimgrid_monthly(2021, "prcp", "conus", wide = FALSE, verbose = TRUE)
+n_p <- get_nclimgrid_normals("1901-2000", "prcp", "conus", wide = FALSE, verbose = TRUE)
+a_p <- compute_anomaly(m_p, n_p)
+
+m_p %>%
+  filter(month %in% 7) %>%
+  plot_nclimgrid(show_credit = F) +
+  theme_void() +
+  theme(legend.position = 'none', strip.text = element_blank()) +
+  labs(title = NULL, subtitle = NULL)
+
+n_p %>%
+  filter(month %in% 5:8) %>%
+  plot_nclimgrid()
+
+a_p %>%
+  filter(month %in% 6) %>%
+  plot_nclimgrid()
 
